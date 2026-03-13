3e4ec70cfc92ced17c81932f015873c0a95991a1
leroy <kerettsleroy@gmail.com>
2026-03-13 15:29:13 +0300

    First commit of the project

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+principal=172000
+rate_1=2.25/100
+rate_2=1.49/100
+time=1
+interest_1=principal*rate_1*(time)/12
+interest_2=principal*rate_2*(time)/12
+print("The total interest is:", interest_1 + interest_2)
